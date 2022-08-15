8a724b4c94aa07a71de0b48de08b0117423f2c71
Art <iartrx@gmail.com>
2022-08-15 16:05:49 +0700

    completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result = 0;
 
+    if((typeof (a && b) == "number") && (a > 0 && b > 0)) {
+        if(a < b){    
+            for(let i = a; i <= b; i++) {
+                result += i;
+            }
+        }else{ 
+            for(let i = b; i <= a; i++) {
+                result += i;
+        }
+        }
+        return result;       
+    }else {
+        return "ERROR";
+    }
+    
+    
 };
 
 // Do not edit below this line
